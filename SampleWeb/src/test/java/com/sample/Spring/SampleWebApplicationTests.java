23235572ce8fd65502a35c77c70e4e40350f4232
Tarun Sarpanjeri <31917612+dexterstr@users.noreply.github.com>
2020-04-16 18:41:22 -0500

    initial commit

---
+++ SampleWeb/src/test/java/com/sample/Spring/SampleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
