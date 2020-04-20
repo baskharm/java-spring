d5ebc93bccc3051559076467968eade4a39aefed
bhaskar29090 <reddybhaskar437@gmail.com>
2020-04-19 23:35:50 -0500

    first

--- SampleWeb/src/main/java/com/sample/Spring/sController.java
+++ SampleWeb/src/main/java/com/sample/Spring/sController.java
@@ -1,12 +1,10 @@
-package com.sample.Spring;
-
-import org.springframework.stereotype.Controller;
+package com.sample.Spring;import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class sController {
 
-	@RequestMapping("tarun")
+	@RequestMapping("bro")
 	public String index() {
 		return "index.html";
 		
